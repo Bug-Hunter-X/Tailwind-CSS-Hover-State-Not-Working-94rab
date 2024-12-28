b245d27e73e37ec2d816aaac5f41c86449987bd4
BugHunter-X <askzenai@gmail.com>
2024-12-28 22:10:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+//some tailwindcss classes are not working as expected
+<div class="bg-red-500 hover:bg-blue-700 p-4 rounded-lg">
+  <p>This button should change color on hover</p>
+</div>
+```
